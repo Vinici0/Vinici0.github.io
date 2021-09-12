207e9422cdc3b8b376e33fc1257d8459dad2639b
Vinici0 <75345399+Vinici0@users.noreply.github.com>
2021-09-12 14:48:02 -0500

    Prueba

---
+++ JavaScript/login.js
@@ -0,0 +1,11 @@
+const registrarButton = document.getElementById('registro');
+const iniciarButton = document.getElementById('iniciarS');
+const container = document.getElementById('container');
+
+registrarButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+iniciarButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Vinici0.github.io
-Portafolio
+# FundamentosPrueba
